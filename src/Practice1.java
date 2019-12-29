0bf0b6632b6afaac3811f184b5abf7654dc02779
Kristaps <kristaps.auzans@gmail.com>
2019-12-29 23:37:14 +0200

    First commit

--- src/Practice1.java
+++ src/Practice1.java
@@ -1,4 +1,3 @@
-import java.lang.Math;
 
 public class Practice1 {
     public static void main(String[] args) {
@@ -46,25 +45,20 @@ public class Practice1 {
     public static int calculateHighScorePosition(int playerScore) {
         if (playerScore >= 1000) {
             return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
+        } else if (playerScore >= 500) {
             return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
+        } else if (playerScore >= 100) {
             return 3;
         } else {
             return 4;
         }
 
-
     }
-
 
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
         System.out.println(playerName + " is at highscore position: " + highScorePosition + " On the highscore table!");
 
     }
-
-    //PW2
-    //PW3
 
 
 }
