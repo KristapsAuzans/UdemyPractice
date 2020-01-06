b4a0d55e18da75ac6231e0be8df74d525ad08b01
Kristaps <kristaps.auzans@gmail.com>
2020-01-06 22:04:36 +0200

    Decimal Excercise

---
+++ src/Excercises1/DecimalComparator.java
@@ -0,0 +1,27 @@
+package Excercises1;
+
+public class DecimalComparator {
+    public static void main(String[] args) {
+        areEqualByThreeDecimalPlaces(-3.1756, -3.175);
+        areEqualByThreeDecimalPlaces(3.175, 3.176);
+        areEqualByThreeDecimalPlaces(3.0, 3.0);
+        areEqualByThreeDecimalPlaces(-3.123, 3.123);
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double first, double second) {
+        first = first * 1000;
+        second = second * 1000;
+        int checkFirst = (int) first;
+        int checkSecond = (int) second;
+        boolean testOutcome = false;
+
+        if (checkFirst == checkSecond) {
+            testOutcome = true;
+        } else {
+
+            testOutcome = false;
+        }
+        return testOutcome;
+    }
+
+}
