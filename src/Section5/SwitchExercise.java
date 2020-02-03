a728b3cbf2b0f21bf651888fad917c5448ae0bb3
Kristaps <kristaps.auzans@gmail.com>
2020-02-03 22:54:06 +0200

    Section 5, switch

---
+++ src/Section5/SwitchExercise.java
@@ -0,0 +1,33 @@
+package Section5;
+
+public class SwitchExercise {
+    public static void main(String[] args) {
+        char letter = 'C';
+        switch (letter) {
+            case 'A':
+                System.out.println("This is letter A");
+                break;
+
+            case 'B':
+                System.out.println("This is letter B");
+                break;
+
+            case 'C':
+                System.out.println("This is letter C");
+                break;
+
+            case 'D':
+                System.out.println("This is letter D");
+                break;
+
+            case 'E':
+                System.out.println("This is letter E");
+                break;
+
+            default:
+                System.out.println("Neither of the letter");
+                break;
+        }
+
+    }
+}
