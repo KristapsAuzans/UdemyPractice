edde574f8bf5668eedb7d5bf80a41f4779d6e125
KristapsAuzans <kristaps.auzans@gmail.com>
2020-03-26 14:53:31 +0200

    SpeedConverter Excercise3 - Barking dog

---
+++ src/Section5/EvenNumber.java
@@ -0,0 +1,35 @@
+package Section5;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+
+        int number = 4;
+        int finishNumber = 20;
+        int count = 0;
+        while (number <= finishNumber) {
+            number++;
+
+            if (!isEvenNumber(number)) {
+                count++;
+                if (count <= 5) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            System.out.println("Even number " + number);
+        }
+    }
+
+    private static boolean isEvenNumber(int number) {
+        boolean check = false;
+
+        if (number % 2 == 0) {
+            check = true;
+        } else {
+        }
+        return check;
+    }
+
+
+}
