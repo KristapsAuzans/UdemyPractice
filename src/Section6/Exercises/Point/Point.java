1be71af9836498e57f9802087090bc23266b169b
KristapsAuzans <kristaps.auzans@gmail.com>
2020-05-17 13:39:22 +0300

    Point_2

--- src/Section6/Exercises/Point/Point.java
+++ src/Section6/Exercises/Point/Point.java
@@ -4,4 +4,48 @@ public class Point {
     private int x;
     private int y;
 
+
+    public Point() {
+        this(0, 0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return distance(0, 0);
+    }
+
+    public double distance(int x, int y) {
+       // double distance = Math.sqrt((getX() - x) * (getX() - x) + (getY() - y) * (getY() - y));
+      double distance= Math.sqrt((x-this.x)*(x-this.x)+(y-this.y)*(y-this.y));
+        return distance;
+
+    }
+
+    public double distance(Point point) {
+        int a = point.getY();
+        int b = point.getX();
+        return distance(a, b);
+
+    }
+
 }
