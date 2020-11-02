a2de21c34b91af57c3e1b0d570204b2fb8e48954
KristapsAuzans <kristaps.auzans@gmail.com>
2020-11-02 17:15:49 +0200

    Interfaces

---
+++ src/Section9/Main.java
@@ -0,0 +1,71 @@
+package Section9;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Player kris = new Player("Kris", 56, 13);
+        System.out.println(kris.toString());
+        savedObject(kris);
+        System.out.println(kris);
+        kris.setWeapon("Hunters bow");
+        savedObject(kris);
+        loadObject(kris);
+        System.out.println(kris);
+
+
+        ISaveable zombie = new Monster("Zombie", 230, 9);
+        System.out.println("Strength = "+((Monster)zombie).getStrength());
+        System.out.println(zombie);
+        savedObject(zombie);
+
+
+    }
+
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<String>();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a string\n" +
+                "0 to quit");
+
+        while (!quit) {
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.print("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+
+    public static void savedObject(ISaveable objectToSave) {
+        for (int i = 0; i < objectToSave.write().size(); i++) {
+            System.out.println("Saving " + objectToSave.write().get(i) + " to storage device");
+        }
+
+    }
+
+    public static void loadObject(ISaveable objectToLoad){
+        ArrayList<String> values = readValues();
+        objectToLoad.read(values);
+
+    }
+
+
+}
